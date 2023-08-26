653ed9dbbc9f09a332479db1dcf97bde7f7b2ec6
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-08-26 16:57:06 +0530

    final change

--- API-basics/app.js
+++ API-basics/app.js
@@ -1,21 +1,20 @@
 import express from "express";
-import userRouter from "./routes/user.js"
-
+import userRouter from "./routes/user.js";
+import { config } from "dotenv";
 
 export const app = express();
+
+config({
+  // path: __dirname + "/.env",
+  path: "./data/config.env"
+})
 
 // using middleware
 app.use(express.json());
 app.use(userRouter);
-
-
 
 // CREATING ROUTES
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-
-
-
--- API-basics/data/database.js
+++ API-basics/data/database.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 // DATABASE CONNECTION
 
-const DB =
-  "mongodb+srv://ananya:pandathedon@database01.3djalcx.mongodb.net/contact?retryWrites=true&w=majority";
+const DB = process.env.MONGO_URI;
 
 export const connectDB = () => {
   mongoose
